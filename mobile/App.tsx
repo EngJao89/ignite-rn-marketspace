3300974aea2167d0bdddd1b8af6056d1d0ec8abf
EngJao89 <eng_joao@outlook.com>
2023-05-15 09:39:53 -0300

    feat(ms07-loading): implementa componente de loading

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,13 @@
-import { StatusBar } from 'react-native'
-import { useFonts, Karla_400Regular, Karla_700Bold } from '@expo-google-fonts/karla'
+import { StatusBar } from 'react-native';
+import { useFonts, Karla_400Regular, Karla_700Bold } from '@expo-google-fonts/karla';
 
-import { NativeBaseProvider } from 'native-base'
-import { theme } from './src/theme'
+import { NativeBaseProvider } from 'native-base';
+import { theme } from './src/theme';
 
-import { Loading } from '@components/Loading'
-import { Routes } from '@routes/index'
+import { Loading } from '@components/Loading';
+import { Routes } from '@routes/index';
 
-import { AuthContextProvider } from '@contexts/AuthContext'
+import { AuthContextProvider } from '@contexts/AuthContext';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold })
