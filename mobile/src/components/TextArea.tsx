60c3f4e519c0d2a0b8a1ffbc050d884dbaf54e60
EngJao89 <eng_joao@outlook.com>
2023-05-22 16:02:57 -0300

    feat(ms21-createad): implementa tela de criação dos anúncios

---
+++ mobile/src/components/TextArea.tsx
@@ -0,0 +1,38 @@
+import {
+  FormControl,
+  ITextAreaProps,
+  TextArea as NativeBaseTextArea,
+} from 'native-base'
+
+interface Props extends ITextAreaProps {
+  errorMessage?: string | null
+}
+
+export function TextArea({ errorMessage = null, isInvalid, ...rest }: Props) {
+  const invalid = !!errorMessage || isInvalid
+
+  return (
+    <FormControl isInvalid={invalid} mb={4}>
+      <NativeBaseTextArea
+        bg="gray.700"
+        numberOfLines={5}
+        h={40}
+        p={4}
+        borderWidth={0}
+        placeholder="Descrição do produto"
+        placeholderTextColor="gray.400"
+        fontSize="md"
+        color="gray.200"
+        fontFamily="regular"
+        _focus={{
+          borderWidth: 1,
+          borderColor: 'gray.300',
+          bg: 'gray.700',
+        }}
+        autoCompleteType={undefined}
+        {...rest}
+      />
+      <FormControl.ErrorMessage>{errorMessage}</FormControl.ErrorMessage>
+    </FormControl>
+  )
+}
